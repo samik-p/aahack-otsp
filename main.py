b4028a147a5b7e9ffd3f8103109f2298940770e7
Samik Pradhan <samikpradhan@gmail.com>
2023-08-19 21:20:48 -0700

    implemented creation of new playlist using search results from query

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ if __name__ == "__main__":
     query = input("\nEnter a search query: ")
 
     search_results = yt_search(youtube, query)
+    video_ids = []
     print("\nSearch Results:")
-    # print(search_results)
     for idx, result in enumerate(search_results):
+        video_ids.append(result["video_id"])
         print(
             f"{idx}: {result['title']}\n\thttps://www.youtube.com/watch?v={result['video_id']}"
         )
 
-    description = yt_get_video_description(youtube, search_results[0]["video_id"])
-    print(f"\nVideo Description for {search_results[0]['title']}:\n\n{description}")
+    print()
+
+    playlist_title = input("Enter a title for this playlist: ")
+    playlist_description = (
+        f"Playlist created from the search results of the following query: {query}"
+    )
+
+    playlist_id = yt_create_playlist(youtube, playlist_title, playlist_description)
+
+    yt_add_videos_to_playlist(youtube, playlist_id, video_ids)
+
+    # description = yt_get_video_description(youtube, search_results[0]["video_id"])
+    # print(f"\nVideo Description for {search_results[0]['title']}:\n\n{description}")
